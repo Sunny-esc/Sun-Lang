fea275346b9f421603a153b0a09106cf73eb685d
Sunny-esc <sunnydubey8511330@gmail.com>
2026-03-20 14:15:32 +0530

    Interpreter done

---
+++ Sun/Interpreter.java
@@ -0,0 +1,175 @@
+package Sun;
+
+/*The class declares that it’s a visitor. 
+The return type of the visit methods will be Object, the root class 
+that we use to refer to a Lox value in our Java code.*/
+class Interpreter implements Expr.Visitor<Object> {
+/*----------------------------------------------------------------------------------------------------*/
+/*------------------------------------MAIN------------------------------------------------------------*/
+//this method  takes in a syntax tree for an expression and evaluates it. 
+    // If that succeeds, evaluate() returns an object for the result value
+    void interpret(Expr expression) { 
+    try {
+      Object value = evaluate(expression);
+
+      System.out.println(stringify(value));
+    } catch (RuntimeError error) {
+      Lox.runtimeError(error);
+    }
+  }
+/*----------------------------------------------------------------------------------------------------*/
+    // now we convert the literal tree node into a runtime value
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
+
+    /* Evaluating unary expressions */
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+
+        // we evaluate the operand expression.
+        // Then we apply the unary operator itself to the result of that.
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case BANG:
+                return !isTruthy(right);
+            case MINUS:
+                // -, which negates the result of the subexpression.
+                // The subexpression must be a number. Since we don’t statically know that in
+                // Java, we cast it before performing the operation.
+                return -(double) right;
+        }
+
+        // Unreachable.
+        return null;
+    }
+
+    // runtime error detection for minus opp
+    private void checkNumberOperand(Token operator, Object operand) {
+        if (operand instanceof Double)
+            return;
+        throw new RuntimeError(operator, "Operand must be a number.");
+    }
+
+    private void checkNumberOperands(Token operator,
+            Object left, Object right) {
+        if (left instanceof Double && right instanceof Double)
+            return;
+
+        throw new RuntimeError(operator, "Operands must be numbers.");
+    }
+
+    // explaination in the page of Evaluting expr 5 heading
+    private boolean isTruthy(Object object) {
+        if (object == null)
+            return false;
+        if (object instanceof Boolean)
+            return (boolean) object;
+        return true;
+    }
+
+    // This is one of those corners where the details of how we represent Lox
+    // objects in terms of Java matter.
+    // We need to correctly implement Lox’s notion of equality, which may be
+    // different from Java’s.
+    private boolean isEqual(Object a, Object b) {
+        if (a == null && b == null)
+            return true;
+        if (a == null)
+            return false;
+
+        return a.equals(b);
+    }
+
+     private String stringify(Object object) {
+    if (object == null) return "nil";
+
+    if (object instanceof Double) {
+      String text = object.toString();
+      if (text.endsWith(".0")) {
+        text = text.substring(0, text.length() - 2);
+      }
+      return text;
+    }
+
+    return object.toString();
+  }
+
+    // A grouping node has a reference to an inner node for the expression contained
+    // inside the parenthese
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+    // method which simply sends the expression back into the interpreter’s visitor
+    // implementation:
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+
+    // Evaluating binary operators
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+        // The main difference from the unary negation operator is that we have two
+        // operands to evaluate
+        switch (expr.operator.type) {
+            case GREATER:
+                checkNumberOperands(expr.operator, left, right);
+
+                return (double) left > (double) right;
+            case GREATER_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
+
+                return (double) left >= (double) right;
+            case LESS:
+                checkNumberOperands(expr.operator, left, right);
+
+                return (double) left < (double) right;
+            case LESS_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
+
+                return (double) left <= (double) right;
+            case MINUS:
+                // Detecting runtime errors
+                checkNumberOperands(expr.operator, left, right);
+
+                return (double) left - (double) right;
+
+            // + operator can also be used to concatenate two strings.
+            // To handle that, we don’t just assume the operands are a certain type and cast
+            // them,
+            // we dynamically check the type and choose the appropriate operation.
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double) left + (double) right;
+                }
+
+                if (left instanceof String && right instanceof String) {
+                    return (String) left + (String) right;
+                }
+    throw new RuntimeError(expr.operator,
+            "Operands must be two numbers or two strings.");
+            case SLASH:
+                checkNumberOperands(expr.operator, left, right);
+
+                return (double) left / (double) right;
+            case STAR:
+                checkNumberOperands(expr.operator, left, right);
+
+                return (double) left * (double) right;
+            case BANG_EQUAL:
+                return !isEqual(left, right);
+            case EQUAL_EQUAL:
+                return isEqual(left, right);
+        }
+
+        // Unreachable.
+        return null;
+    }
+
+}
